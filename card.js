cdefbeeeb07a2f2c68e12c8a0966f71e5fe06022
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-05-09 13:39:48 -0500

    Inheritance on card.js now using Object.create to achieve this. Enumeration type for card types added. Constructors for Card and MonsterCard added

--- card.js
+++ card.js
@@ -1,6 +1,39 @@
-function Card(){
-    this.title = ''
-    this.description = ''
-    this.card_number = 0
-    this.picture = ''
+function Card(params){
+    this.title = params && params['title'] || '';
+    this.description = params && params['description'] || '';
+    this.card_number = params && params['card_number'] || '';
+    this.rarity = params && params['rarity'] || '';
+    this.picture = params && params['picture'] || '';
+    this.type = params && params['type'] || '';
+    this.subtype = params &&  params['sub_type'] || '';
 }
+
+//Enumeration
+Card.ECardType = {
+     MONSTER: 'monster'
+    , TRAP: 'trap'
+    , SPELL: 'spell'
+};
+
+function MonsterCard(params){
+    Card.call(this,params);
+    this.attribute = params && params['attribute'] || '';
+    this.effect = params && params['effect'] || ''; // none | flip |
+    this.level = params && params['level'] || '';
+    this.atk = params && params['atk'] || '';
+    this.def = params && params['def'] || '';
+}
+MonsterCard.prototype = Object.create(Card.prototype);
+
+
+
+function SpellCard(){
+    this.property = ''; // normal | continuous | ritual | quick-play | field | equip
+}
+SpellCard.prototype = Object.create(Card.prototype);
+
+
+function TrapCard(){
+    this.property = ''; // normal | continuous | counter
+}
+TrapCard.prototype = Object.create(Card.prototype);
