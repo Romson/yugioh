cd33f86cf3efe910afcfcb922eee153b442a5d11
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-05-09 14:27:08 -0500

    Correction for cards.js for missing inheretance details on either Spell and Trap card objects. New pseudo-enumeration types added.

--- card.js
+++ card.js
@@ -1,22 +1,22 @@
-function Card(params){
+function Card(params) {
     this.title = params && params['title'] || '';
     this.description = params && params['description'] || '';
     this.card_number = params && params['card_number'] || '';
     this.rarity = params && params['rarity'] || '';
     this.picture = params && params['picture'] || '';
     this.type = params && params['type'] || '';
-    this.subtype = params &&  params['sub_type'] || '';
+    this.subtype = params && params['sub_type'] || '';
 }
 
 //Enumeration
 Card.ECardType = {
-     MONSTER: 'monster'
+    MONSTER: 'monster'
     , TRAP: 'trap'
     , SPELL: 'spell'
 };
 
-function MonsterCard(params){
-    Card.call(this,params);
+function MonsterCard(params) {
+    Card.call(this, params);
     this.attribute = params && params['attribute'] || '';
     this.effect = params && params['effect'] || ''; // none | flip |
     this.level = params && params['level'] || '';
@@ -26,14 +26,27 @@ function MonsterCard(params){
 MonsterCard.prototype = Object.create(Card.prototype);
 
 
-
-function SpellCard(){
-    this.property = ''; // normal | continuous | ritual | quick-play | field | equip
+function SpellCard(params) {
+    Card.call(this, params);
+    this.property = params && params['property'] || ''; // normal | continuous | ritual | quick-play | field | equip
 }
 SpellCard.prototype = Object.create(Card.prototype);
+SpellCard.ECardProperty = {
+    NORMAL: 'normal'
+    , CONTINUOUS: 'continuous'
+    , RITUAL: 'ritual'
+    , QUICK_PLAY: 'quick-play'
+    , FIELD: 'field'
+    , EQUIP: 'equip'
+};
 
-
-function TrapCard(){
-    this.property = ''; // normal | continuous | counter
+function TrapCard(params) {
+    Card.call(this, params);
+    this.property = params && params['property'] || ''; // normal | continuous | counter
 }
 TrapCard.prototype = Object.create(Card.prototype);
+TrapCard.ECardProperty = {
+    NORMAL: 'normal'
+    , CONTINUOUS: 'continuous'
+    , COUNTER: 'counter'
+};
